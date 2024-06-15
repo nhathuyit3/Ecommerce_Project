f7a4d3143c34a2c5b60decab4e9581b1c2b1589f
nhathuyit3 <hnhuy.17it3@sict.udn.vn>
2024-06-15 18:48:52 +0700

    config backend

--- Backend/ecommerce_core/ecommerce_api/ecommerce_api/settings.py
+++ Backend/ecommerce_core/ecommerce_api/ecommerce_api/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ecommerce_api_core.apps.EcommerceApiCoreConfig'    
 ]
 
 MIDDLEWARE = [
---
+++ Backend/ecommerce_core/ecommerce_api/ecommerce_api_core/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EcommerceApiCoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ecommerce_api_core'
